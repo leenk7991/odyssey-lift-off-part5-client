c22995cef037ab709d17a56c98804029ef3248b1
Leen <leen.kilani@gmail.com>
2021-11-11 13:02:43 +0200

    update uri

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import GlobalStyles from './styles';
-import Pages from './pages';
-import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
+import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
+import React from "react";
+import ReactDOM from "react-dom";
+import Pages from "./pages";
+import GlobalStyles from "./styles";
 
 const client = new ApolloClient({
-  uri: 'http://localhost:4000',
+  uri: "https://server-catsronauts1.herokuapp.com/",
   cache: new InMemoryCache(),
 });
 
@@ -14,5 +14,5 @@ ReactDOM.render(
     <GlobalStyles />
     <Pages />
   </ApolloProvider>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
